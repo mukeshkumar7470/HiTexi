515d3dbeb8cea5965fc428d109525c9831837a31
pc <kumarmukeshpatel57@gmail.com>
2021-03-08 16:07:47 +0530

    Initial commit

---
+++ app/src/main/java/com/mukeshdev/hitexi/ui/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.mukeshdev.hitexi.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mukeshdev.hitexi.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
---
+++ app/src/main/java/com/mukeshdev/hitexi/ui/SplashScreenActivity.java
@@ -0,0 +1,38 @@
+package com.mukeshdev.hitexi.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+
+import com.mukeshdev.hitexi.MainActivity;
+import com.mukeshdev.hitexi.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        initSplash();
+
+    }
+
+    private void initSplash() {
+       new Handler().postDelayed(new Runnable() {
+           @Override
+           public void run() {
+               /* Create an Intent that will start the Menu-Activity. */
+               Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+               startActivity(mainIntent);
+               finish();
+           }
+       }, SPLASH_DISPLAY_LENGTH);
+    }
+}
